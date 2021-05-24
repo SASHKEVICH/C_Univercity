d9641231a156cfd7150bb5df831080499c56bb7f
Aleksander Bekrenev <praise2001@yandex.ru>
2021-05-24 15:21:06 +0700

    Update home9.c

--- home9.c
+++ home9.c
@@ -5,25 +5,42 @@
 
 void createArray(int *array, int length);
 void printArray(int *array, int length);
-void permutation(int *array, int length);
-void shift(int *array, int length, int m);
+int permutation(int *array, int length);
+void shift(int *array, int *tempArray, int length, int lastIndex);
 
 int main()
 {
-    int length = 4;
-    // printf("Длина массива: ");
-    // scanf("%d", &length);
-    // int *array = (int *)malloc(sizeof(int) * length);
+    int length;
+    printf("Длина массива: ");
+    scanf("%d", &length);
+    int *array = (int *)calloc(sizeof(int), length + 1);
 
-    int array[4] = {1, 2, 3, 4};
 
-    createArray(array, length);
-    printf("Оригинальный массив:\n");
-    printArray(array, length);
+    printf("Создать множество от 0 до n (1) или произвольное (2)?\n");
+    printf("Введите 1 или 2:\n");
+    int choice;
+    scanf("%d", &choice);
+    if(choice == 1)
+    {
+        createArray(array, length);    
+    }
+    else 
+    {
+        printf("Множество: \n");
+        for(int i = 1; i < length + 1; i++)
+        {   
+            int temp;
+            scanf("%d", &temp);
+            array[i] = temp;
+        }
+    }
 
-    printf("Тест:\n");
-    permutation(array, length);
-    // printArray(array, length);
+    printf("--------------------\n");
+    printf("Перестановки:\n");
+
+    int per = permutation(array, length);
+    printf("Перестановок - %d", per);
+    free(array);
 
     return 0;
 }
@@ -31,53 +48,56 @@ int main()
 // Заполнение массива элементами от 1 до n
 void createArray(int *array, int length)
 {
-    for(int i = 0; i < length; i++)
+    for(int i = 1; i < length + 1; i++)
     {
-        array[i] = i + 1;
+        array[i] = i;
     }
 }
 
 void printArray(int *array, int length)
 {
-    for(int i = 0; i < length; i++)
+    for(int i = 1; i < length + 1; i++)
     {
         printf("%d ", array[i]);
     }
     printf("\n");
 }
 
-void permutation(int *array, int length)
+int permutation(int *array, int length)
 {
-    int lastIndex = 0;
-    int *tempArray = (int *)malloc(sizeof(int) * length);
-    int temp;
-
-    for(int i = 0; i < length; i++)
-    {
-        tempArray[i] = array[i];
-    }
+    int lastIndex;
+    int per = 0;
+    int *tempArray = (int *)calloc(sizeof(int), length + 1);
+    createArray(tempArray, length);
 
     while(lastIndex != 1)
     {
+        lastIndex = length;
         printArray(array, length);
-        lastIndex = length - 1;
-        shift(array, length, lastIndex);
-        while((array[lastIndex] == lastIndex) && (lastIndex > 0))
+        shift(array, tempArray, length, lastIndex);
+        while((tempArray[lastIndex] == lastIndex) && (lastIndex > 1))
         {
             lastIndex--;
-            shift(array, length, lastIndex);
-            printArray(array, length);
+            shift(array, tempArray, length, lastIndex);
         }
+        per++;
     }
+    free(tempArray);
+    return per;
 }
 
 // Алгоритм перестановки
-void shift(int *array, int length, int m)
+void shift(int *array, int *tempArray, int length, int lastIndex)
 {
-    int temp = array[m - 1];
-    for(int n = length - 2; n >= 0; n--)
+
+    int temp1 = tempArray[lastIndex];
+    int temp2 = array[lastIndex];
+
+    for(int n = lastIndex - 1; n > 0; n--)
     {
+        tempArray[n + 1] = tempArray[n];
         array[n + 1] = array[n];
     }
-    array[0] = temp;
+    tempArray[1] = temp1;
+    array[1] = temp2;
 }
